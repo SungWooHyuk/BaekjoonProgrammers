d3099354d77b06c6cb194ccc6c31bad59944024e
WOOHYUKSUNG <94315575+SungWooHyuk@users.noreply.github.com>
2024-09-26 00:19:44 +0900

    [level 1] Title: 공원 산책, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.cpp
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.cpp
@@ -1,105 +1,59 @@
-// 지나다니는 길을 O 장애물 X
-// 공원 바운더리 + 장애물 체크 걸리면 다음 명령 수행
 #include <string>
 #include <vector>
 #include <utility>
-#include <iostream>
+#include <unordered_map>
 using namespace std;
 
 vector<int> solution(vector<string> park, vector<string> routes) {
     vector<int> answer;
-    pair<int, int> pos;
+    pair<int,int> pos;
     
-    for(int i = 0; i < park.size(); ++i)
+    for(int i = 0; i < park.size(); ++i){
         for(int j = 0; j < park[i].size(); ++j)
-            if(park[i][j] == 'S')
-                pos = make_pair(i,j);
-    
-    for(int i = 0; i < routes.size(); ++i)
-    {
-        char c = routes[i][0];
-        int move = routes[i].back() - '0';
-        
-        // 범위검사
-        if(c == 'W'){
-            if(pos.second - move < 0)
-                continue;
-        }
-        else if(c== 'N'){
-            if(pos.first - move < 0)
-                continue;
-        }
-        else if(c== 'E'){
-            if(pos.second + move > park[0].size()-1)
-                continue;
-        }
-        else{
-            if(pos.first + move > park.size() -1)
-                continue;
-        }
-    
-        int point = 0;
-        
-        while(move > 0)
         {
-            if(c == 'W')
+            if(park[i][j] == 'S')
             {
-                if(park[pos.first][pos.second-1] != 'X')
-                {
-                    point++;
-                    pos.second -= 1;
-                    move--;  
-                }
-                else{
-                    pos.second += point;
-                    break;
-                }
-            }
-            else if(c == 'N')
-            {
-                if(park[pos.first-1][pos.second] != 'X')
-                {
-                    point++;
-                    pos.first -= 1;
-                    move--;
-                }
-                else
-                {
-                    pos.first += point;
-                    break;
-                }
-            }
-            else if(c == 'E')
-            {
-                if(park[pos.first][pos.second+1] != 'X')
-                {
-                    point++;
-                    pos.second += 1;
-                    move--;
-                }
-                else
-                {
-                    pos.second -= point;
-                    break;
-                }
-            }
-            else
-            {
-                if(park[pos.first+1][pos.second] != 'X')
-                {
-                    point++;
-                    pos.first += 1;
-                    move--;
-                }
-                else
-                {
-                    pos.first -= point;
-                    break;
-                }
+                pos = {i,j};
+                break;
             }
         }
     }
     
+    unordered_map<char, pair<int,int>> directions = {
+        {'N', {-1,0}}, {'S', {1,0}}, {'W',{0, -1}}, {'E', {0,1}}  
+    };
+    
+    for(auto r : routes)
+    {
+        char direction = r[0];
+        int move = r.back() - '0';
+        
+        int dx = directions[direction].first;
+        int dy = directions[direction].second;
+        
+        int nx = pos.first;
+        int ny = pos.second;
+        
+        bool valid = true;
+        
+        for(int step = 0; step < move; ++step){
+            nx += dx;
+            ny += dy;
+            
+            if(nx < 0 || nx >= park.size() || ny < 0 || ny >= park[0].size() ||
+              park[nx][ny] == 'X')
+            {
+                valid = false;
+                break;
+            }
+        }
+        
+        if(valid)
+        {
+            pos.first = nx;
+            pos.second = ny;
+        }
+    }
     answer.push_back(pos.first);
     answer.push_back(pos.second);
     return answer;
