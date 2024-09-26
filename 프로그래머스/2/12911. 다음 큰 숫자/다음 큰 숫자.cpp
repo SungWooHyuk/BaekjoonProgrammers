1a14275ea8924b3f039bdd239af90a65d91fe202
WOOHYUKSUNG <94315575+SungWooHyuk@users.noreply.github.com>
2024-09-26 13:33:26 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 3.76 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
@@ -1,37 +1,23 @@
-// n의 다음 큰 숫자는 n보다 큰 자연수
-// n과 n+1은 2진수로 변환했을때 1의 갯수가 같음. 0001   0010  0100   1000    1  2   4   8 
-// n의 다음 큰 숫자는 최소임
-// 78 - 1001110
-// 83 - 1010011
-
-#include <string>
-#include <vector>
 #include <iostream>
 using namespace std;
 
-int solution(int n) {
-    int first_n = n;
-    int cnt = 0;
-    while(first_n > 0)
-    {
-        int remain = first_n%2;
-        if(remain == 1)
-            cnt++;
-        first_n /= 2;
+// 이진수에서 1의 개수를 계산하는 함수
+int countOnes(int num) {
+    int count = 0;
+    while (num > 0) {
+        count += num % 2;
+        num /= 2;
     }
-    int cnt_s = 0;
-    while(cnt != cnt_s)
-    {
-        cnt_s = 0;
-        n++;
-        int second_s = n;
+    return count;
+}
 
-        while(second_s > 0)
-        {
-            int remain = second_s%2;
-            if(remain == 1)
-                cnt_s++;
-            second_s /= 2;
+int solution(int n) {
+    int cnt = countOnes(n); // n의 1의 개수
+    
+    while (true) {
+        n++;
+        if (cnt == countOnes(n)) { // 1의 개수가 같으면 종료
+            break;
         }
     }
     
