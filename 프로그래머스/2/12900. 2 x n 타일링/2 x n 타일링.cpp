c916580b64b8499a2577296f20096d356784c0b8
WOOHYUKSUNG <94315575+SungWooHyuk@users.noreply.github.com>
2024-10-04 01:24:33 +0900

    [level 2] Title: 2 x n 타일링, Time: 0.30 ms, Memory: 3.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    vector<int> vec(n+1);
+    vec[1] = 1;
+    vec[2] = 2;
+    
+    for(int i = 3; i <= n; ++i)
+    {
+        vec[i] = (vec[i-1] + vec[i-2])% 1000000007;
+    }
+    return vec[n];
+}
